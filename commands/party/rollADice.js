14f6ce0c5548fe14579c50d09ebb1ae3a6e099a4
Paulo <paulo-augusto12@outlook.com>
2024-01-21 19:23:48 -0300

    feat: added custom value option

--- commands/party/rollADice.js
+++ commands/party/rollADice.js
@@ -1,4 +1,9 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+} = require("discord.js");
 
 function diceResultDescription(result) {
   if (result < 10) {
@@ -14,6 +19,10 @@ function diceResultDescription(result) {
     return "THIS WAS AN SUPERB RESULT 💫💫";
   }
 }
+
+function getARandomSide(dice) {
+  return Math.floor(Math.random() * dice - 1 + 1);
+}
 const embeds = (user, dice, result) => {
   return new EmbedBuilder()
     .setTitle(`${user.username} rolled a ${dice}`)
@@ -22,47 +31,65 @@ const embeds = (user, dice, result) => {
     )
     .setDescription(`🎲 the dice result was ${result}`)
     .setColor("Purple")
-    .setFooter({text: diceResultDescription(result)})
+    .setFooter({ text: diceResultDescription(result) });
 };
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("roll-dice")
     .setDescription("Will roll a dice")
-    .addStringOption((option) =>
+    .addNumberOption((option) =>
       option
-        .setRequired(true)
-        .setName("dice")
+        .setName("default-dices")
         .setDescription("Choose a dice")
         .addChoices(
-          { name: "D6", value: "d6" },
-          { name: "D12", value: "d12" },
-          { name: "D20", value: "d20" }
+          { name: "D6", value: 6 },
+          { name: "D12", value: 12 },
+          { name: "D20", value: 20 }
         )
+    )
+    .addStringOption((option) =>
+      option
+        .setName("custom-value")
+        .setRequired(false)
+        .setDescription("Add a custom value")
     ),
 
   async execute(interaction) {
-    const dice = interaction.options.getString("dice");
+    const dice = interaction.options.getNumber("default-dices");
+    const custom = interaction.options.getString("custom-value");
 
     const rollDice = () => {
       switch (dice) {
-        case "d6": {
-          return Math.floor(Math.random() * 6 - 1 + 1);
+        case 6: {
+          return getARandomSide(dice);
         }
-        case "d12": {
-          return Math.floor(Math.random() * 12 - 1 + 1);
+        case 12: {
+          return getARandomSide(dice);
         }
-        case "d20": {
-          return Math.floor(Math.random() * 20 - 1 + 1);
+        case 20: {
+          return getARandomSide(dice);
         }
       }
     };
 
-    // `🎲 ${
-    //   interaction.user.username
-    // } Rolled a ${dice.toUpperCase()} and the result was ${rollDice()}`
+    if (custom) {
+      if (Number.isInteger(parseInt(custom))) {
+        return await interaction.reply({
+          embeds: [
+            embeds(
+              interaction.user,
+              custom.toUpperCase(),
+              Math.floor(Math.random() * custom - 1 + 1)
+            ),
+          ],
+        });
+      } else {
+        return await interaction.reply("Please type a number");
+      }
+    }
     return await interaction.reply({
-      embeds: [embeds(interaction.user, dice.toUpperCase(), rollDice())],
+      embeds: [embeds(interaction.user, `D${dice}`, rollDice())],
     });
   },
 };
