3557a48a012bf7b1d68a104a33b9eabf7a943065
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-22 09:58:35 -0300

    refactor: changed the code to portuguese and removed default dice sides

--- commands/party/rollADice.js
+++ commands/party/rollADice.js
@@ -1,82 +1,40 @@
-const {
-  SlashCommandBuilder,
-  EmbedBuilder
-} = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
-function getARandomSide(dice) {
-  return Math.floor(Math.random() * dice - 1 + 1);
-}
 const embeds = (user, dice, result) => {
   return new EmbedBuilder()
-    .setTitle(`${user.username} rolled a D${dice}`)
+    .setTitle(`${user.username} rolou um D${dice}`)
     .setThumbnail(
       `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
     )
-    .setDescription(`🎲 the dice result was ${result}`)
+    .setDescription(`🎲 O resultado do dado foi ${result}`)
     .setColor("Purple")
     .addFields(
-      { name: "Minimun", value: "1", inline: true },
-      { name: "Maximun", value: `${dice}`, inline: true },
-      { name: "Result", value: `${result}`, inline: true }
-    ).setTimestamp(new Date().getTime())
+      { name: "Mínimo", value: "1", inline: true },
+      { name: "Maximo", value: `${dice}`, inline: true },
+      { name: "Resultado", value: `${result}`, inline: true }
+    )
+    .setTimestamp(new Date().getTime());
 };
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("roll-dice")
-    .setDescription("Will roll a dice")
+    .setDescription("Faça uma rolagem de um dado")
     .addNumberOption((option) =>
       option
-        .setName("default-dices")
-        .setDescription("Choose a dice")
-        .addChoices(
-          { name: "D6", value: 6 },
-          { name: "D12", value: 12 },
-          { name: "D20", value: 20 }
-        )
-    )
-    .addStringOption((option) =>
-      option
-        .setName("custom-value")
-        .setRequired(false)
-        .setDescription("Add a custom value")
+        .setName("lados")
+        .setRequired(true)
+        .setDescription("A quantidade de lados do dado")
     ),
 
   async execute(interaction) {
-    const dice = interaction.options.getNumber("default-dices");
-    const custom = interaction.options.getString("custom-value");
+    const dice = interaction.options.getNumber("lados");
 
     const rollDice = () => {
-      switch (dice) {
-        case 6: {
-          return getARandomSide(dice);
-        }
-        case 12: {
-          return getARandomSide(dice);
-        }
-        case 20: {
-          return getARandomSide(dice);
-        }
-      }
+      return Math.floor(Math.random() * dice - 1 + 1);
     };
-
-    if (custom) {
-      if (Number.isInteger(parseInt(custom))) {
-        return await interaction.reply({
-          embeds: [
-            embeds(
-              interaction.user,
-              custom.toUpperCase(),
-              Math.floor(Math.random() * custom - 1 + 1)
-            ),
-          ],
-        });
-      } else {
-        return await interaction.reply("Please type a number");
-      }
-    }
     return await interaction.reply({
-      embeds: [embeds(interaction.user, `D${dice}`, rollDice())],
+      embeds: [embeds(interaction.user, `${dice}`, rollDice())],
     });
   },
 };
