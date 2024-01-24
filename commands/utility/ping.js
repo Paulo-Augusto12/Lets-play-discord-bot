aad8696ed20be008808a9328e640276201ee6342
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-24 17:30:11 -0300

    feat: redeveloping ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,21 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { getUserAvatarUrl } = require("../../utils/getUserAvatarUrl");
 
+const embeds = (user, latency) => {
+  new EmbedBuilder()
+    .setTitle("Latência")
+    .setAuthor({
+      name: user.username,
+      iconURL: getUserAvatarUrl(user.avatar, user.id),
+    })
+    .addFields({ name: "Latência", value: `${latency}ms` });
+};
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('responde pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("confere a latência do bot"),
+  async execute(interaction) {
+    const latency = Date.now() - interaction.createdTimestamp;
+    await interaction.reply({ embeds: [embeds(interaction.user, latency)] });
+  },
 };
