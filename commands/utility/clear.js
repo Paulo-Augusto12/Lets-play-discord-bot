f0c42c4614b946f4df06af266716fd76c0f1b364
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-24 11:10:09 -0300

    feat: created utilitary function to delete bot messages from a channel

---
+++ commands/utility/clear.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, userMention, inlineCode } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("clear")
+    .setDescription("Vou limpar parte da bagunça que fiz neste canal")
+    .addNumberOption((option) =>
+      option
+        .setName("qtde")
+        .setDescription("Quantas mensagens quer que eu exclua")
+        .setMaxValue(100)
+        .setMinValue(1)
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const channel = interaction.channel;
+    const messages = await channel.messages.fetch({
+      before: interaction.id,
+      limit: 100,
+    });
+    const qtde = interaction.options.getNumber("qtde");
+
+    messages.forEach(async (message) => {
+      await message.delete();
+    });
+
+    interaction.reply(
+      `${userMention(interaction.user.id)} serão deletadas ${inlineCode(qtde)} mensagens que enviei neste canal !`
+    );
+  },
+};
