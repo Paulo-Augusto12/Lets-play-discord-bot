6b185d5fc2c296713f2e2c47b3910debcbfb0ad5
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-24 14:34:18 -0300

    feat: added a styled embed for /user command

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,13 +1,45 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  userMention,
+  time
+} = require("discord.js");
+const { getUserAvatarUrl } = require("../../utils/getUserAvatarUrl");
 
+const embeds = (user, guild, joinedAt) => {
+  return new EmbedBuilder()
+    .setAuthor({
+      name: user.username,
+      iconURL: getUserAvatarUrl(user.avatar, user.id),
+    })
+    .setColor("Purple")
+    .setImage(
+      user.banner
+        ? `https://cdn.discordapp.com/${user.id}/${user.banner}.png`
+        : getUserAvatarUrl(user.avatar, user.id)
+    )
+    .setThumbnail(getUserAvatarUrl(user.avatar, user.id))
+    .setTimestamp(new Date().getTime())
+    .setDescription(`Confira um pouco sobre o usuário ${userMention(user.id)}`)
+    .addFields(
+      { name: "menção", value: userMention(user.id), inline: true }
+    ).addFields({ name: "Entrou neste canal em", value: time(new Date(joinedAt)) });
+};
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("user")
     .setDescription("Will retur information about the user"),
 
   async execute(interaction) {
-    await interaction.reply(
-      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
-    );
+    console.log(interaction.member.roles);
+    await interaction.reply({
+      embeds: [
+        embeds(
+          interaction.user,
+          interaction.guild,
+          interaction.member.joinedTimestamp
+        ),
+      ],
+    });
   },
 };
