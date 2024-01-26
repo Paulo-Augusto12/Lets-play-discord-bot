08a958daef9d00b31557bddef01aa2e0c704c146
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-01-26 10:21:06 -0300

    feat: ping command returning latency

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,14 +1,15 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, blockQuote } = require("discord.js");
 const { getUserAvatarUrl } = require("../../utils/getUserAvatarUrl");
 
 const embeds = (user, latency) => {
-  new EmbedBuilder()
-    .setTitle("Latência")
+  return new EmbedBuilder()
+    .setTitle("Confira a minha latência para responder seus comandos")
     .setAuthor({
       name: user.username,
       iconURL: getUserAvatarUrl(user.avatar, user.id),
     })
-    .addFields({ name: "Latência", value: `${latency}ms` });
+    .setColor("Purple")
+    .addFields({ name: "Latência ⏱️", value: ` ${blockQuote(latency)}ms` });
 };
 module.exports = {
   data: new SlashCommandBuilder()
@@ -16,6 +17,7 @@ module.exports = {
     .setDescription("confere a latência do bot"),
   async execute(interaction) {
     const latency = Date.now() - interaction.createdTimestamp;
+    console.log(interaction.createdTimestamp);
     await interaction.reply({ embeds: [embeds(interaction.user, latency)] });
   },
 };
