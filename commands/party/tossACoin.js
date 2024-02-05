10d4743f423c6bb2ce8b20ddc4834ebb76fadc2d
Paulo Augusto Ribeiro <paulo-augusto12@outlook.com>
2024-02-05 15:14:36 -0300

    feat: added option to deal in toss a coin command

--- commands/party/tossACoin.js
+++ commands/party/tossACoin.js
@@ -3,13 +3,33 @@ const {
   EmbedBuilder,
   userMention,
   spoiler,
-  ButtonBuilder,
-  ActionRowBuilder,
-  ButtonStyle,
+  codeBlock,
+  inlineCode,
 } = require("discord.js");
 const { getUserAvatarUrl } = require("../../utils/getUserAvatarUrl");
+const { supabase } = require("../../server/server");
 
-const embeds = (user, choice, result) => {
+async function getUserFromSupabase(userId) {
+  const { data } = await supabase.from("users").select().eq("id", userId);
+  if (data) {
+    return data[0];
+  }
+}
+
+async function registerDealValueInDatabase(userId, dealValue, action) {
+  const user = await getUserFromSupabase(userId);
+
+  const { error } = await supabase
+    .from("users")
+    .update({
+      play_points:
+        action === "increase"
+          ? user.play_points + dealValue
+          : user.play_points - dealValue,
+    })
+    .eq("id", userId);
+}
+const embeds = (user, choice, result, dealData) => {
   const avatar = getUserAvatarUrl(user.avatar, user.id);
   return new EmbedBuilder()
     .setAuthor({
@@ -17,9 +37,18 @@ const embeds = (user, choice, result) => {
       iconURL: avatar,
     })
     .setTitle("Lancei a moeda !")
-    .setDescription(
-      `${userMention(user.id)} escolheu ${choice} e... ${spoiler(result)}`
+    .setDescription(`${userMention(user.id)} escolheu ${choice} e... ${result}`)
+    .addFields(
+      {
+        name: `Play points foram apostados nesse jogo !`,
+        value: `Você apostou ${inlineCode(dealData.deal)} Play points`,
+      },
+      {
+        name: `Você ${result}`,
+        value: `E então foram ${result === "venceu" ? "adicionados" : "retirados"} ${inlineCode(dealData.deal)} play points na sua conta`,
+      }
     )
+    .setTimestamp(Date.now())
     .setThumbnail(avatar);
 };
 module.exports = {
@@ -35,26 +64,58 @@ module.exports = {
           { name: "Coroa", value: "coroa" }
         )
         .setRequired(true)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("aposta")
+        .setDescription(
+          "Insira uma quantidade de play points para apostar na moeda"
+        )
     ),
 
   async execute(interaction) {
     const side = interaction.options.getString("side");
+    const dealValue = interaction.options.getNumber("aposta");
+    const databaseUser = await getUserFromSupabase(interaction.user.id);
+    const userCannotMakeADeal = databaseUser.play_points < dealValue;
 
-    const getAOption = () => {
+    if (userCannotMakeADeal) {
+      return await interaction.reply(
+        `${userMention(interaction.user.id)} você não tem a quantidade de play points inserida para apostar ! atualmente você tem ${codeBlock(databaseUser.play_points)}`
+      );
+    }
+    const getAOption = async () => {
       const options = ["cara", "coroa"];
 
       const result =
         options[Math.floor(Math.random() * options.length - 1 + 1)];
 
       if (side === result) {
-        return "venceu !";
+        await registerDealValueInDatabase(
+          interaction.user.id,
+          dealValue,
+          "increase"
+        );
+        return "venceu";
       } else {
-        return "perdeu !";
+        await registerDealValueInDatabase(
+          interaction.user.id,
+          dealValue,
+          "decrease"
+        );
+        return "perdeu";
       }
     };
 
+    const gameResult = await getAOption();
+
     return await interaction.reply({
-      embeds: [embeds(interaction.user, side, getAOption())],
+      embeds: [
+        embeds(interaction.user, side, gameResult, {
+          deal: dealValue,
+          result: gameResult,
+        }),
+      ],
     });
   },
 };
